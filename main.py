0c0e1c5a4b9fac66722234ec1411508eabf9a4c9
Thiago Vasconcelos <pedrosav14@gmail.com>
2022-11-10 17:17:43 -0300

    updated_todo_by_id and create_todo functions implemented and get_todos_by_id handling exception updated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 
@@ -37,9 +38,32 @@ async def home():
     return todos
 
 
-@app.get("/todo/{id_todo}")
-async def get_todos(id_todo: int):
+@app.get("/todo/{id_todo}", response_model=Todo)
+async def get_todos_by_id(id_todo: int):
     for todo in todos:
         id_actual: int = todo.get("id")
         if id_actual == id_todo:
             return todo
+        else:
+            raise HTTPException(status_code=404, detail="Item not found")
+
+@app.put("/todo/", response_model=Todo)
+async def update_todo_by_id(id_todo: int, todo: Todo):
+    try:
+        updated_todo = jsonable_encoder(todo)
+        todos[id_todo] = updated_todo
+        return f'Updated todo at {id_todo} position: {updated_todo}'
+    except TypeError:
+        return 'TypeError found'
+    finally:
+        return f'All Todos: \n{todos}'
+
+
+@app.post("/todo/", response_model=Todo)
+async def create_todo(todo: Todo):
+    try:
+        todos.append(todo)
+    except TypeError:
+        return TypeError
+    finally:
+        return todo
