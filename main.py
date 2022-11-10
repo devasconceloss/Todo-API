d642b8d71db41a134d06d3cb24e5fcc7b7100ad2
Thiago Vasconcelos <pedrosav14@gmail.com>
2022-11-10 18:27:37 -0300

    Fixed the type error for todos being a list, implemented httpexception for update by id and get by id todo's function

--- main.py
+++ main.py
@@ -4,26 +4,9 @@ from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 
-todos = [
-    {
-        "id": 1,
-        "title": "Read a Book",
-        "category": "Other",
-        "done": False
-    },
-    {
-        "id": 2,
-        "title": "Go to the gym",
-        "category": "Health",
-        "done": True
-    },
-    {
-        "id": 3,
-        "title": "Watch Mr Robot",
-        "category": "Other",
-        "done": False
-    }
-]
+todos = {
+
+}
 
 
 class Todo(BaseModel):
@@ -40,29 +23,26 @@ async def home():
 
 @app.get("/todo/{id_todo}", response_model=Todo)
 async def get_todos_by_id(id_todo: int):
-    for todo in todos:
-        id_actual: int = todo.get("id")
-        if id_actual == id_todo:
-            return todo
-        else:
-            raise HTTPException(status_code=404, detail="Item not found")
+    if id_todo in todos.keys():
+        return todos[id_todo]
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
 
-@app.put("/todo/", response_model=Todo)
+
+@app.put("/todo/{id_todo}", response_model=Todo)
 async def update_todo_by_id(id_todo: int, todo: Todo):
-    try:
+    if id_todo in todos.keys():
         updated_todo = jsonable_encoder(todo)
-        todos[id_todo] = updated_todo
-        return f'Updated todo at {id_todo} position: {updated_todo}'
-    except TypeError:
-        return 'TypeError found'
-    finally:
-        return f'All Todos: \n{todos}'
+        todo[id_todo] = updated_todo
+        return updated_todo
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
 
 
 @app.post("/todo/", response_model=Todo)
 async def create_todo(todo: Todo):
     try:
-        todos.append(todo)
+        todos[len(todos)] = todo
     except TypeError:
         return TypeError
     finally:
