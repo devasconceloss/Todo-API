d0bb457c7a817a1e9409c16c34551cae5260eec1
Thiago Vasconcelos <pedrosav14@gmail.com>
2022-12-12 18:43:53 -0300

    settings file is exclusive for database path configuration

--- settings.py
+++ settings.py
@@ -1,10 +1,22 @@
 import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+from pathlib import Path
+
+env_path = Path('.env')
+load_dotenv(dotenv_path=env_path)
 
 
-database_url = os.getenv('database_url')
-engine = create_engine(database_url)
-local_session = sessionmaker(autocommit=False, autoflush=False)
-Base = declarative_base()
+class DatabaseConfiguration:
+    project_name: str = "TodoAPI + Postgres"
+    project_version: str = "Version 1.1.0"
+
+    Postgres_user:      str = os.getenv("POSTGRES_USER")
+    Postgres_pass:      str = os.getenv("POSTGRES_PASS")
+    Postgres_server:    str = os.getenv("POSTGRES_SERVER")
+    Postgres_port:      str = os.getenv("POSTGRES_PORT")
+    Postgres_database:  str = os.getenv("POSTGRES_DATABASE")
+    Database_url = f'postgresql://{Postgres_user}:{Postgres_pass}' \
+                   f'@{Postgres_server}:{Postgres_port}/{Postgres_database}'
+
+
+settings = DatabaseConfiguration()
