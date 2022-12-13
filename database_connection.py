263591843c197ca19ea5c03b4bcbfc25082288c6
Thiago Vasconcelos <pedrosav14@gmail.com>
2022-12-13 14:29:24 -0300

    Added declarative base

--- database_connection.py
+++ database_connection.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 from settings import settings
 
 sqlalchemy_db_url = settings.Database_url
 
 engine = create_engine(sqlalchemy_db_url)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
