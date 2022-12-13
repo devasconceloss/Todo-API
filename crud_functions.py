adca33b39da5643ffa6eedd75d6926f9e186cda3
Thiago Vasconcelos <pedrosav14@gmail.com>
2022-12-13 14:29:04 -0300

    Implemented all Crud basic functions

---
+++ crud_functions.py
@@ -0,0 +1,44 @@
+from sqlalchemy.orm import Session
+from models import Todo
+from schemas import TodoSchema
+
+
+def get_todos(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(Todo).offset(skip).limit(limit).all()
+
+
+def get_todo_by_id(db: Session, todo_id: int):
+    return db.query(Todo).filter(Todo.id == todo_id).first()
+
+
+def create_todo(db: Session, todo: TodoSchema):
+    new_todo = Todo(
+        id=todo.id,
+        title=todo.title,
+        category=todo.category,
+        done=todo.done
+    )
+
+    db.add(new_todo)
+    db.commit()
+    db.refresh(new_todo)
+    return new_todo
+
+
+def deleted_todo(db: Session, todo_id: int):
+    todo_to_be_deleted = get_todo_by_id(db=db, todo_id=todo_id)
+    db.delete(todo_to_be_deleted)
+    db.commit()
+
+
+def updated_todo(db: Session, todo_id: int, title: str, category: str, done: bool):
+    todo_to_be_updated = get_todo_by_id(db=db, todo_id=todo_id)
+
+    todo_to_be_updated.id = todo_id
+    todo_to_be_updated.title = title
+    todo_to_be_updated.category = category
+    todo_to_be_updated.done = done
+
+    db.commit()
+    db.refresh(todo_to_be_updated)
+    return todo_to_be_updated
