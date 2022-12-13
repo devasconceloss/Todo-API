3d3192061268703536fae1ce7a196960bcbedb35
Thiago Vasconcelos <pedrosav14@gmail.com>
2022-12-13 14:30:37 -0300

    including router endpoints

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from database_connection import engine
+import models
+from endpoints import *
+
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.include_router(router, tags=["todo"])
