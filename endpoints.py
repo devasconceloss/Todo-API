9aaa4a3c592755eae518f5c54f4454e9d022ef21
Thiago Vasconcelos <pedrosav14@gmail.com>
2022-12-14 22:27:30 -0300

    Updated the responses and add patch endpoint

--- endpoints.py
+++ endpoints.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from database_connection import SessionLocal
 from sqlalchemy.orm import Session
 import crud_functions
@@ -20,8 +20,8 @@ def get_data_base():
 async def get_all_todos(skip: int = 0, limit: int = 100, db: Session = Depends(get_data_base)):
     todolist = crud_functions.get_todos(db, skip, limit)
     return Response(
-        status="200",
-        code="Ok",
+        status="OK",
+        code="200",
         message="Fetched all data",
         result=todolist
     )
@@ -30,45 +30,59 @@ async def get_all_todos(skip: int = 0, limit: int = 100, db: Session = Depends(g
 @router.get("/todos/{todo_id}", tags=["todos"])
 async def get_todo_by_id(todo_id: int, db: Session = Depends(get_data_base)):
     selected_todo: TodoSchema = crud_functions.get_todo_by_id(db=db, todo_id=todo_id)
-    return Response(
-        status="200",
-        code="OK",
-        message="Fetched the selected data",
-        result=selected_todo
-    )
+    if selected_todo:
+        return Response(
+            status="OK",
+            code="200",
+            message="Fetched the selected data",
+            result=selected_todo
+        )
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
 
 
 @router.post("/todos", tags=["todos"])
 async def create_todo(request: RequestTodo, db: Session = Depends(get_data_base)):
     crud_functions.create_todo(db=db, todo=request.parameter)
     return Response(
-        status="201",
-        code="Created",
+        status="Created Todo",
+        code="201",
         message="Todo created successfully"
     )
 
 
-@router.delete("/", tags=["todos"])
-async def deleting_todo(request: RequestTodo, db: Session = Depends(get_data_base)):
-    crud_functions.deleted_todo(db=db, todo_id=request.parameter.id)
+@router.delete("/todos/{todo_id}")
+async def delete_book(todo_id: int,  db: Session = Depends(get_data_base)):
+    crud_functions.deleted_todo(db, todo_id=todo_id)
     return Response(
-        status="204",
-        code="No content"
+        status="OK",
+        code="200",
+        message="Todo deleted"
     )
 
 
+@router.patch("/todos/{todo_id}")
+async def updating_todo(request: RequestTodo, todo_id: int, db: Session = Depends(get_data_base)):
+    todo_to_be_updated: TodoSchema = crud_functions.get_todo_by_id(db, todo_id=request.parameter.id)
+
+    return todo_to_be_updated
+
+
 @router.put("/todos/{todo_id}")
-async def updating_todo(request: RequestTodo, db: Session = Depends(get_data_base)):
-    updated_todo = crud_functions.updated_todo(
-        db,
+async def finishing_todo(request: RequestTodo, db: Session = Depends(get_data_base)):
+    todo_done = crud_functions.finishing_todo(
+        db=db,
         todo_id=request.parameter.id,
         title=request.parameter.title,
         category=request.parameter.category,
         done=request.parameter.done
     )
 
-    return Response(
-        status="202",
-        code="Accepted",
-        result=updated_todo
-    )
+    if todo_done:
+        return Response(
+            status="Accepted",
+            code="202",
+            result=todo_done
+        )
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
