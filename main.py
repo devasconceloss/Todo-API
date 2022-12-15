5f7b8d4843bade75f7119cdb16fcc5c62046b686
Thiago Vasconcelos <pedrosav14@gmail.com>
2022-12-14 22:33:23 -0300

    Added middlewares, tried to add in a different file but is getting ASGI error

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ from fastapi import FastAPI
 from database_connection import engine
 import models
 from endpoints import *
+from fastapi.middleware.cors import CORSMiddleware
 
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(router, tags=["todo"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
